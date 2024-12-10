832ff437d59612253780c3c5cd2772c07d15490c
Keith Nicholson <knicholson@ksdynamics.com>
2024-12-09 21:16:58 -0600

    Adding LabelHandlder for FontFamily

--- Telerik_PDFViewer_issue/MauiProgram.cs
+++ Telerik_PDFViewer_issue/MauiProgram.cs
@@ -23,6 +23,27 @@ namespace Telerik_PDFViewer_issue
     		builder.Logging.AddDebug();
 #endif
 
+//#if WINDOWS10_0_17763_0_OR_GREATER
+//    Microsoft.Maui.Handlers.LabelHandler.Mapper.AppendToMapping("FontFamily", (handler, element) =>
+//    {
+//        /*  An unpackaged app does not have ms-appx://Assets/ folder available. To avoid issues you can explicitly define the new path to the font file
+//         *
+//         *  - Packaged => "ms-appx://Assets/Fonts/Font.ttf#familyname" 
+//         *  - Unpackaged => "font.ttf#familyname"
+//         *
+//         *  In order for the unpackaged approach to work, be sure to include the font's file with the project's assets and set the Build Action to 'CopyAlways'
+//        */
+
+//        handler.PlatformView.FontFamily = element.Font.Family.ToLower() switch
+//        {
+//            "telerikcontrolsicons" => new Microsoft.UI.Xaml.Media.FontFamily("telerikcontrolsicons.ttf#telerikcontrolsicons"),
+//            "telerikfontexamples" => new Microsoft.UI.Xaml.Media.FontFamily("telerikfontexamples.ttf#telerikfontexamples"),
+//            _ => handler.PlatformView.FontFamily
+//        };
+//    });
+//#endif
+
+
             return builder.Build();
         }
     }
